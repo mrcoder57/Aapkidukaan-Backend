c4b2689b5d42ab3b6b0360883639d47e7bedf9af
mrcoder57 <tiwariji2300@gmail.com>
2023-11-13 22:10:51 +0530

    feat: deplyoment ready

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import http from 'http';
 import productsRoutes from './Routes/productRoutes.js'
 import userRoutes from './Routes/userRoutes.js';
 import cors from 'cors';
+import { config } from 'dotenv';
+config();
+
 const app = express();
 
 app.use(cors());
@@ -13,8 +16,9 @@ app.get('/home', (req, res) => {
   res.send('Hello World!');
 });
 app.use(express.json());
-server.listen(5000, () => {
-  console.log('Server running on port 5000');
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
   app.use('/', productsRoutes);
   app.use('/user', userRoutes);
 
